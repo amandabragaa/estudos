261ba678003b31f31a3a369c451a5cbb6958f91d
amandabraga <amandabraga1998@gmail.com>
2025-04-15 10:29:50 -0300

    Adiciona o diretório aula04, estudos direcionados aos operadores

---
+++ cdc.js/Aula04-Operadores/aritheticOperadors.js
@@ -0,0 +1,36 @@
+
+// salário 
+// soma
+const salario = 5000;
+const valeAlimentacao = 500;
+
+console.log("O salário total (incluindo VA) é: ", salario + valeAlimentacao);
+
+// subtração
+const salarioBruto = 7000;
+const descontos = 2000;
+
+console.log("O salário que cai na conta é: ", salarioBruto - descontos);
+
+// casamento da Amanda
+// multiplicação
+const valorBuffetPorPeessoa = 80;
+const totalConvidados = 100;
+
+console.log("O valor total d Buffet é: ", valorBuffetPorPeessoa * totalConvidados);
+
+// divisão
+const  custoTotalCasamento = 80000;
+
+console.log("O valor por pessoa é: ", custoTotalCasamento / totalConvidados);
+
+console.log("-----------------------------------------")
+
+// números parese impares (módulo)
+let numero = 10
+console.log("O resto da divisão de " + numero + " por 2 é:", numero % 2);
+console.log("O numero " + numero + " é PAR");
+
+numero = 11;
+console.log("O resto da divisão de " + numero + " por 2 é:", numero % 2);
+console.log("O numero " + numero + " é IMPAR");
